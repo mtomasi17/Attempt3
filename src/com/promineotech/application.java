c4523a88f4c1863d94ff39364dc60f9e43ba3100
Mike Tomasi <mthunter1@hotmail.com>
2023-05-18 17:39:24 -0400

    pushing testOutPage

---
+++ src/com/promineotech/application.java
@@ -0,0 +1,31 @@
+package com.promineotech;
+
+public class application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		double savingsAccountBalance = 29.00;
+//		double depositAmount = 100.00;
+//		double withdrawalAmount = 50.00;
+//		double newSavingsAccountBalance = 0.00;
+//		
+//		//Ballance Inquiry
+//		System.out.println(String.format("The balance in your account is: $%.2f", savingsAccountBalance));
+//		//Deposit Check
+//		newSavingsAccountBalance = savingsAccountBalance + depositAmount;
+//		System.out.println(String.format("You have deposited $%.2f", depositAmount));
+//		System.out.println(String.format("Your new balance is $%.2f", newSavingsAccountBalance));
+//		//Withdraw Cash
+//		newSavingsAccountBalance = newSavingsAccountBalance - withdrawalAmount;
+//		System.out.println(String.format("Here is your withdrawal of $%.2f", withdrawalAmount));
+//		System.out.println(String.format("Your new balance is $%.2f", newSavingsAccountBalance));
+		
+		//System.out.println((-2 < -1) || ((10 > 3) && false));
+		int i = 1;
+		do{
+		    System.out.println(i);
+		    i++;
+		} while (i <= 10);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mthun
+ *
+ */
+module testOutPage {
+}
